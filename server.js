e39307086d4ec4464a6d41e7e2672894f62b40b6
darbic <tomas.klima@aiteq.com>
2017-05-22 09:40:15 +0200

    comments added

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+/*
+ * aiteq/nodejs-http-proxy
+ * Copyright (c) 2017 Aiteq Reloaded, s.r.o. <info@aiteq.com>
+ * MIT Licensed
+ */
+
+/**
+ * Module dependencies
+ */
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const request = require('request');
 
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());                  // enable Cross-origin resource sharing
 app.use(bodyParser.json());       // enable JSON-encoded bodies
@@ -11,12 +22,12 @@ app.use(bodyParser.urlencoded({   // enable URL-encoded bodies
   extended: true
 })); 
 
-// handle testing GET request
+// install handler for testing GET request
 app.get("/test", (req, res) => {
   res.send("Test success");
 });
 
-// handle proxy POST request
+// install handler for proxy requests
 app.post("/proxy", (req, res) => {
 
   if (!req.body.url) {
@@ -27,24 +38,20 @@ app.post("/proxy", (req, res) => {
   var options = {
     uri: req.body.url,
     method: 'POST',
-    json: req.body
+    json: req.body  // re-post all data
   };
 
-  request(options, (error, response, body) => {
+  // request target url
+  request(options, (error, remoteResponse, remoteResponseBody) => {
 
     error && console.error(error);
 
-    res.status(response.statusCode).send(body);
-
-    /*
-    if (!error && response.statusCode == 200) {
-      res.send(body); // Send the response of the requested url to the frontend.
-    }
-    */
+    // send untouched response to the origin
+    res.status(remoteResponse.statusCode).send(remoteResponseBody);
   })
 });
 
-const PORT = process.env.PORT || 8080;
+// start the server
 app.listen(PORT, () => {
   console.log("App listening on port " + PORT);
 });
